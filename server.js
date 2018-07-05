7578be8e757156d3867fcdf0277e8a84d9b643dd
Ashleigh Barnard <ashleighnbarnard@gmail.com>
2018-07-05 16:08:11 -0400

    mongodb integration

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//Connect to mongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err => console.log('Mongo Error')));
 
 //route to get up and running
 app.get('/', (req, res) => res.send('Hello World'));
