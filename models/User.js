20ad1eb57ac8eb51b7c1b40b298e422fdc15ce27
Ashleigh Barnard <ashleighnbarnard@gmail.com>
2018-07-05 17:26:49 -0400

    login, passport auth, validation

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// create schema
+// Create Schema
 const UserSchema = new Schema({
   name: {
     type: String,
@@ -16,13 +16,12 @@ const UserSchema = new Schema({
     required: true
   },
   avatar: {
-    type: String,
-    required: true
+    type: String
   },
   date: {
     type: Date,
     default: Date.now
-  },
+  }
 });
 
-module.export = User = mongoose.model('users, User');
+module.exports = User = mongoose.model('users', UserSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+//@route          GET api/profile/test
+//@description    Tests the profile route 
+//@access         Public 
 router.get('/test', (req, res) => res.json({
   msg: 'Profile is working'
 }));
